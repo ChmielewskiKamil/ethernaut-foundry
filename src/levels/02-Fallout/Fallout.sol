b58a6d967e555b8fa5fce9ec04c7f8668cb04fe6
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-10-01 01:42:54 +0200

    🔎 audit: add audit bookmarks to the Fallout contract

--- src/levels/02-Fallout/Fallout.sol
+++ src/levels/02-Fallout/Fallout.sol
@@ -8,6 +8,11 @@ contract Fallout {
     mapping(address => uint) allocations;
     address payable public owner;
 
+    /**
+     * @audit-issue typo in the constructor name
+     * because of that Fallout contract has no constructor
+     * Fal1out function can be called by anyone
+     */
     /* constructor */
     function Fal1out() public payable {
         owner = msg.sender;
@@ -19,15 +24,20 @@ contract Fallout {
         _;
     }
 
+    // @audit weird syntax mapping.add?
+    // @audit no event emitted on critical function
     function allocate() public payable {
         allocations[msg.sender] = allocations[msg.sender].add(msg.value);
     }
 
+    // @audit no event emitted on critical function
     function sendAllocation(address payable allocator) public {
         require(allocations[allocator] > 0);
         allocator.transfer(allocations[allocator]);
     }
 
+    // @audit no timelock -> possible unexpected withdrawal
+    // @audit no event emitted on critical function
     function collectAllocations() public onlyOwner {
         msg.sender.transfer(address(this).balance);
     }
