3869acbf513073496750e798c8dec255bdbdb5e7
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-09-30 20:02:03 +0200

    🎨 style: format code

--- src/core/Ethernaut.sol
+++ src/core/Ethernaut.sol
@@ -32,11 +32,7 @@ contract Ethernaut is Ownable {
     event LevelInstanceCreatedLog(address indexed player, address instance);
     event LevelCompletedLog(address indexed player, Level level);
 
-    function createLevelInstance(Level _level)
-        public
-        payable
-        returns (address)
-    {
+    function createLevelInstance(Level _level) public payable returns (address) {
         // Ensure level is registered.
         require(registeredLevels[address(_level)]);
 
@@ -44,11 +40,7 @@ contract Ethernaut is Ownable {
         address instance = _level.createInstance{value: msg.value}(msg.sender);
 
         // Store emitted instance relationship with player and level.
-        emittedInstances[instance] = EmittedInstanceData(
-            msg.sender,
-            _level,
-            false
-        );
+        emittedInstances[instance] = EmittedInstanceData(msg.sender, _level, false);
 
         // Retrieve created instance via logs.
         emit LevelInstanceCreatedLog(msg.sender, instance);
@@ -56,10 +48,7 @@ contract Ethernaut is Ownable {
         return instance; // Return data - not possible to read emitted events via solidity
     }
 
-    function submitLevelInstance(address payable _instance)
-        public
-        returns (bool)
-    {
+    function submitLevelInstance(address payable _instance) public returns (bool) {
         // Get player and level.
         EmittedInstanceData storage data = emittedInstances[_instance];
         require(data.player == msg.sender); // instance was emitted for this player
