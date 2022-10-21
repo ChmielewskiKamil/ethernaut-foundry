3c4589ff3defd33839e2db7752a6bea8b12dbefb
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-10-21 16:38:33 +0200

    🔎 audit: add audit bookmarks

--- src/levels/06-Delegation/Delegation.sol
+++ src/levels/06-Delegation/Delegation.sol
@@ -4,16 +4,20 @@ pragma solidity ^0.6.0;
 contract Delegate {
     address public owner;
 
+    // @audit no 0 address check, if you forget to set the address
+    // the contract might be locked forever
     constructor(address _owner) public {
         owner = _owner;
     }
 
+    // @audit unprotected state modifying function
     function pwn() public {
         owner = msg.sender;
     }
 }
 
 contract Delegation {
+    // @audit-ok the order of state variable declarations is ok
     address public owner;
     Delegate delegate;
 
@@ -23,8 +27,14 @@ contract Delegation {
     }
 
     fallback() external {
+        // @audit dangerous delegatecall with data provided by the user
         (bool result, ) = address(delegate).delegatecall(msg.data);
+
+        // @audit-issue return value is not checked
+
+        // @audit this does nothing(?) -> redundant code
         if (result) {
+            // @follow-up there is no return statement
             this;
         }
     }
