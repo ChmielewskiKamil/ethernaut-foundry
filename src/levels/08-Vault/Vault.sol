0b9d839a26ee1f3726d2e0f3115c15c689a0ff59
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-10-25 21:53:24 +0200

    🔎 audit: add audit bookmarks

--- src/levels/08-Vault/Vault.sol
+++ src/levels/08-Vault/Vault.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.6.0;
 
 contract Vault {
     bool public locked;
+    // @audit nothing is private on the blockchain
+    // passwords especially
     bytes32 private password;
 
     constructor(bytes32 _password) public {
@@ -10,6 +12,9 @@ contract Vault {
         password = _password;
     }
 
+    // @audit-issue password can be accessed by looking at
+    // the contract storage slots
+    // it is not safe
     function unlock(bytes32 _password) public {
         if (password == _password) {
             locked = false;
