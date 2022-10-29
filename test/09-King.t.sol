c0b517aff25eb8657a20400fe0876b165d10da03
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-10-29 15:17:15 +0200

    🚧 test: add level 9 scaffold test

---
+++ test/09-King.t.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+// testing functionalities
+import "forge-std/Test.sol";
+
+// Ethernaut game components
+import "src/core/Ethernaut.sol";
+import "src/levels/09-King/KingFactory.sol";
+import "src/levels/09-King/KingAttack.sol";
+
+contract KingTest is Test {
+    /*//////////////////////////////////////////////////////////////
+                            GAME INSTANCE SETUP
+    //////////////////////////////////////////////////////////////*/
+
+    Ethernaut ethernaut;
+
+    /// @dev eve is the attacker
+    address eve = makeNameForAddress("eve");
+
+    function setUp() public {
+        emit log_string("Setting up King level...");
+        ethernaut = new Ethernaut();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                LEVEL INSTANCE -> EXPLOIT -> SUBMISSION
+    //////////////////////////////////////////////////////////////*/
+
+    function test_KingExploit() public {
+        /*//////////////////////////////////////////////////////////////
+                            LEVEL INSTANCE SETUP
+        //////////////////////////////////////////////////////////////*/
+
+        KingFactory kingFactory = new KingFactory();
+
+        ethernaut.registerLevel(kingFactory);
+        vm.startPrank(eve);
+
+        address levelAddress = ethernaut.createLevelInstance(kingFactory);
+        King kingContract = King(payable(levelAddress));
+
+        /*//////////////////////////////////////////////////////////////
+                                LEVEL EXPLOIT
+        //////////////////////////////////////////////////////////////*/
+
+        /**
+         * CODE GOES HERE
+         */
+
+        /*//////////////////////////////////////////////////////////////
+                                LEVEL SUBMISSION
+        //////////////////////////////////////////////////////////////*/
+
+        bool challengeCompleted = ethernaut.submitLevelInstance(
+            payable(levelAddress)
+        );
+        vm.stopPrank();
+        assert(challengeCompleted);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            HELPER FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * I've found this useful function
+     * in github/twpony Ethernaut repo
+     * @notice This function allows for creating labels (names) for addresses
+     * which will improve readability in traces
+     * @param name you pass the name like "alice" or "bob" and it will create
+     * an address for that person
+     */
+    function makeNameForAddress(string memory name) public returns (address) {
+        address addr = address(
+            uint160(uint256(keccak256(abi.encodePacked(name))))
+        );
+        vm.label(addr, name);
+        return addr;
+    }
+}
