faed16eb7bb3685c5446bbf962353dfcee7f4097
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-10-31 20:39:59 +0100

    🔎 audit: add audit bookmarks

--- src/levels/10-Reentrancy/Reentrance.sol
+++ src/levels/10-Reentrancy/Reentrance.sol
@@ -4,10 +4,17 @@ pragma solidity ^0.6.0;
 import "openzeppelin-contracts/math/SafeMath.sol";
 
 contract Reentrance {
+    //@todo - check if SafeMath is used in every
+    // arithmetic operation
+    //@follow-up - SafeMath is not used in withdraw()
+    // potential underflow
+    //@audit-ok - SafeMath is correclty used
     using SafeMath for uint256;
     mapping(address => uint) public balances;
 
     function donate(address _to) public payable {
+        //@audit-ok - SafeMath is being used
+        // potential overflow - not an issue
         balances[_to] = balances[_to].add(msg.value);
     }
 
@@ -17,10 +24,16 @@ contract Reentrance {
 
     function withdraw(uint _amount) public {
         if (balances[msg.sender] >= _amount) {
+            //@audit - violation of the CEI pattern
+            //@audit-issue - potential high severity reentrancy with eth
             (bool result, ) = msg.sender.call{value: _amount}("");
             if (result) {
                 _amount;
             }
+            //@audit - SafeMath not used
+            // potential integer underflow
+            //@audit-ok - because of the "if" statement
+            // it is not possible to underflow balances
             balances[msg.sender] -= _amount;
         }
     }
