5d3afaa9aae7770b635a44244240207d9686bb70
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-11-07 21:49:20 +0100

    🔎 audit(lvl11): add audit bookmarks

--- src/levels/11-Elevator/Elevator.sol
+++ src/levels/11-Elevator/Elevator.sol
@@ -11,6 +11,16 @@ contract Elevator {
 
     function goTo(uint _floor) public {
         Building building = Building(msg.sender);
+        /**
+         * @audit to pass the level it is necessary to set the
+         * boolean top -> true
+         *
+         * to achieve that we need to pass the following "if" check
+         *
+         * isLastFloor is called 2 times
+         * 1. To get inside the if statement it needs to return false
+         * 2. To set the "top" to true it needs to return true
+         */
 
         if (!building.isLastFloor(_floor)) {
             floor = _floor;
