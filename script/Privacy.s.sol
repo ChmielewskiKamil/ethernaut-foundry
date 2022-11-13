76f0de8bf80d276c8768a0213d17cd46decd9d6c
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-11-13 13:39:53 +0100

    👷 build: finish deployment script, emit lvl addr

--- script/Privacy.s.sol
+++ script/Privacy.s.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.6.0;
 
 import "forge-std/Script.sol";
+// test is needed for emitting events
+import "forge-std/Test.sol";
 
 // Ethernaut game imports
 import "src/core/Ethernaut.sol";
@@ -11,15 +13,22 @@ import "src/core/Level.sol";
 import "src/levels/12-Privacy/Privacy.sol";
 import "src/levels/12-Privacy/PrivacyFactory.sol";
 
-contract MyScript is Script {
+contract Deployment is Script, Test {
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_ANVIL");
+        uint256 deployerPrivateKey = vm.envUint("ANVIL_PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        // NFT nft = new NFT("NFT_tutorial", "TUT", "baseUri");
+        // GAME INSTANCE SETUP //
         Ethernaut ethernaut = new Ethernaut();
 
+        // LEVEL INSTANCE SETUP //
         PrivacyFactory privacyFactory = new PrivacyFactory();
+        ethernaut.registerLevel(privacyFactory);
+
+        address levelAddress = ethernaut.createLevelInstance(privacyFactory);
+        Privacy privacyContract = Privacy(levelAddress);
+
+        emit log_named_address("Level address: ", levelAddress);
 
         vm.stopBroadcast();
     }
