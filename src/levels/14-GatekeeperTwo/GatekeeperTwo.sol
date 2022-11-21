9e3cb9f8a0e8858b38b55edd7c346d0d41017844
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-11-21 19:05:08 +0100

    🔎 audit(lvl14): add audit bookmarks & todos

--- src/levels/14-GatekeeperTwo/GatekeeperTwo.sol
+++ src/levels/14-GatekeeperTwo/GatekeeperTwo.sol
@@ -1,23 +1,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// @audit - There is no SafeMath as in Gatekeeper One -> Sol version ^0.8.0
 contract GatekeeperTwo {
     address public entrant;
 
+    // @audit-ok - this is the exact same modifier as in the previous challenge
     modifier gateOne() {
         require(msg.sender != tx.origin);
         _;
     }
 
+    /*
+     * @todo - does extcodesize return the size of the code at the given address?
+     * @todo - is caller() some assembly specific keyword/function?
+     * does it check for tx.origin or msg.sender or smth else?
+     * @todo - is the syntax x := correct?
+     * what does it do?
+     */
     modifier gateTwo() {
         uint x;
+        // @audit - there is no type safety in assembly
+        // is this safe?
         assembly {
             x := extcodesize(caller())
         }
         require(x == 0);
         _;
     }
-
+    /*
+     * @todo - what does encodePacked(msg.sender) looks like?
+     * encodePacked leaves no padding so... ?
+     *
+     * @audit - is the order of operations correct?
+     * Is it (uint64 ^ uint64) == type(uint64)?
+     * or
+     * Is it (uint64) ^ (uint64 == type(uint64))?
+     *
+     * @todo - is max(uint65) == 2^64 - 1?
+     */
     modifier gateThree(bytes8 _gateKey) {
         require(
             uint64(bytes8(keccak256(abi.encodePacked(msg.sender)))) ^
@@ -27,6 +48,7 @@ contract GatekeeperTwo {
         _;
     }
 
+    // @audit-ok - this is the exact same function as in the previous challenge
     function enter(bytes8 _gateKey)
         public
         gateOne
