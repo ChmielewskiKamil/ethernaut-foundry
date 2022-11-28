d3f5fd07b96b2ac9b2ad057ac7ad37f0a8391bd9
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-11-28 20:52:10 +0100

    ♻️ refactor: to follow best import practices

--- src/levels/14-GatekeeperTwo/GatekeeperTwoFactory.sol
+++ src/levels/14-GatekeeperTwo/GatekeeperTwoFactory.sol
@@ -9,27 +9,22 @@ pragma solidity ^0.8.0;
  * It was necessary to add new OpenZeppelin helpers
  * in the utilities folder
  */
-import "src/utilities/Level-08.sol";
-import "./GatekeeperTwo.sol";
+import {Level} from "src/core/Level-08.sol";
+import {GatekeeperTwo} from "./GatekeeperTwo.sol";
 
 contract GatekeeperTwoFactory is Level {
-    function createInstance(address _player)
-        public
-        payable
-        override
-        returns (address)
-    {
+    function createInstance(
+        address _player
+    ) public payable override returns (address) {
         _player;
         GatekeeperTwo instance = new GatekeeperTwo();
         return address(instance);
     }
 
-    function validateInstance(address payable _instance, address _player)
-        public
-        view
-        override
-        returns (bool)
-    {
+    function validateInstance(
+        address payable _instance,
+        address _player
+    ) public view override returns (bool) {
         GatekeeperTwo instance = GatekeeperTwo(_instance);
         return instance.entrant() == _player;
     }
