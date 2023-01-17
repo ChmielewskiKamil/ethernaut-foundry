441b585358c7388d29bf027a910be65b62bc1699
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-17 19:39:23 +0100

    build: fix imports in factory

--- src/levels/15-naught-coin/NaughtCoinFactory.sol
+++ src/levels/15-naught-coin/NaughtCoinFactory.sol
@@ -1,17 +1,27 @@
- SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
-import "src/utilities/ERC20-08.sol";
+import "src/core/Level-08.sol";
+import "./NaughtCoin.sol";
 
 contract NaughtCoinFactory is Level {
+    function createInstance(address _player)
+        public
+        payable
+        override
+        returns (address)
+    {
+        return address(new NaughtCoin(_player));
+    }
 
-  function createInstance(address _player) override public payable returns (address) {
-    return address(new NaughtCoin(_player));
-  }
-
-  function validateInstance(address payable _instance, address _player) override public view returns (bool) {
-    NaughtCoin instance = NaughtCoin(_instance);
-    return instance.balanceOf(_player) == 0;
-  }
+    function validateInstance(address payable _instance, address _player)
+        public
+        view
+        override
+        returns (bool)
+    {
+        NaughtCoin instance = NaughtCoin(_instance);
+        return instance.balanceOf(_player) == 0;
+    }
 }
